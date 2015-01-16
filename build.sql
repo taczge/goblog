53cfa480584d42ad6cecdba9c9fd0f1847b56404
tn <tn00gm@gmail.com>
2015-01-16 10:34:26 +0900

    modify sql file to insert blog entry for testing.

--- build.sql
+++ build.sql
@@ -3,6 +3,6 @@ create database blog;
 use blog;
 create table entry (id INT auto_increment primary key, title VARCHAR(200) not null, date DATE not null, body TEXT not null);
 
-load data local infile '/home/tn/git/vps/blog/entry.csv' into table entry fields terminated by ',';
+load data local infile 'entry.csv' into table entry fields terminated by ',';
 
 select * from entry;
